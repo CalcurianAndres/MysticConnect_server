3eba2e2adfddc0ff8da268971be020c059cbcdfc
Andrés <calcurianandres@gmail.com>
2025-02-16 15:10:37 -0400

    nueva DB

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI_2, {});
-    console.log('Conexión a MongoDB exitosa en ', process.env.MONGO_URI_2); // Mensaje cuando se conecta
+    console.log('Conexión a MongoDB exitosa'); // Mensaje cuando se conecta
   } catch (err) {
     console.error('Error al conectar a MongoDB:', err.message);
     process.exit(1); // Salir si falla la conexión
