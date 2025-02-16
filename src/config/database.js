370c89ff4e29b84a068d9bedd64161708148e1b5
Andrés <calcurianandres@gmail.com>
2025-02-16 14:57:42 -0400

    cambio DB

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log('Conexión a MongoDB exitosa en ', process.env.MONGO_URI); // Mensaje cuando se conecta
+    await mongoose.connect(process.env.MONGO_URI_2, {});
+    console.log('Conexión a MongoDB exitosa en ', process.env.MONGO_URI_2); // Mensaje cuando se conecta
   } catch (err) {
     console.error('Error al conectar a MongoDB:', err.message);
     process.exit(1); // Salir si falla la conexión
