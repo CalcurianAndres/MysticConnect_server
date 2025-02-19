3b716fc899cd4f9b010b8a309c62d51f571e59fa
Andrés <calcurianandres@gmail.com>
2025-02-19 12:05:40 -0400

    fix bug

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect('mongodb+srv://admin:GidiFczxBTc3t5yK@mysticdb.2m0hd.mongodb.net/', {});
-    // await mongoose.connect(process.env.MONGO_URI_2, {});
-    console.log('Conexión a MongoDB exitosa a mongodb+srv://admin:GidiFczxBTc3t5yK@mysticdb.2m0hd.mongodb.net/'); // Mensaje cuando se conecta
+    await mongoose.connect(process.env.MONGO_URI_2, {});
+    console.log('Conexión a MongoDB exitosa'); // Mensaje cuando se conecta
   } catch (err) {
     console.error('Error al conectar a MongoDB:', err.message);
     process.exit(1); // Salir si falla la conexión
