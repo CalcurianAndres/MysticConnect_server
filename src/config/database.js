878a116a9639d92a5a8022093b91583e9b3914e1
Andrés <calcurianandres@gmail.com>
2025-08-03 15:15:56 -0400

    Last changes

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_2, {});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log('Conexión a MongoDB exitosa'); // Mensaje cuando se conecta
   } catch (err) {
     console.error('Error al conectar a MongoDB:', err.message);
